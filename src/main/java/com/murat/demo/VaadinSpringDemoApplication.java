4db341433089dff2f3f49bef6d3ac297868d4d6b
ozturkmurat <33037786+ozturkmurat@users.noreply.github.com>
2017-10-31 21:44:52 +0300

    Create VaadinSpringDemoApplication.java

---
+++ src/main/java/com/murat/demo/VaadinSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.murat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+public class VaadinSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinSpringDemoApplication.class, args);
+	}
+}
